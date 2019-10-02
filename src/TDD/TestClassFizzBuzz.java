a8a8597f680015a3f3101ce24ea32c2c22bb5b05
Chitsutha <chitsutha@gmail.com>
2019-10-02 16:03:25 +0700

    FizzBuzz version1 : input1 expect1

--- src/TDD/TestClassFizzBuzz.java
+++ src/TDD/TestClassFizzBuzz.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestClassFizzBuzz {
+	FizzBuzz fizzBuzz = new FizzBuzz();
+	int input = fizzBuzz.getInt(1);
 
 	@Test
-	void input_is_1_expect_1() {
-		int input =0;
+	void input_is_1_expect_ouput_is_1() {
 		assertEquals(1, input);
 	}
 
